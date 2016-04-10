4c32aa9991e1ef68f6b9427678d0afab3a9b4916
lkchoi <lester.choi@pith-studio.com>
2016-04-10 18:45:08 -0400

    Further templatize layouts

---
+++ resources/views/layouts/js/flot.blade.php
@@ -0,0 +1,5 @@
+<script src="/js/plugins/flot/jquery.flot.js"></script>
+<script src="/js/plugins/flot/jquery.flot.tooltip.min.js"></script>
+<script src="/js/plugins/flot/jquery.flot.resize.js"></script>
+<script src="/js/plugins/chartJs/Chart.min.js"></script>
+<script src="/js/plugins/peity/jquery.peity.min.js"></script>
