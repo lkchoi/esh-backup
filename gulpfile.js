caad5d55be194f4035a6ea4bd6a5f13859017e1f
lkchoi <lester.choi@pith-studio.com>
2016-05-31 23:30:57 -0400

    Using gulp to watch socket.js and restart the node server was a nice idea, but it just isn't practical because it's too finicky.

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,43 @@
-var gulp = require('gulp');
-var elixir = require('laravel-elixir');
-var spawn = require('child_process').spawn;
-var node;
+var gulp = require('gulp')
+var elixir = require('laravel-elixir')
+var spawn = require('child_process').spawn
+require('laravel-elixir-vueify')
 
-require('laravel-elixir-vueify');
 
 elixir(function(mix) {
     mix .phpUnit()
         .sass('app.scss', 'public/css/app.css')
         .scripts('plugins/*.js', 'public/js/plugins.js')
         .browserify('app.js', 'public/js/app.js')
+        .browserSync({
+            proxy: 'esportshero.app',
+            port: 4000,
+            ui: { port: 4001 }
+        })
+    // gulp.watch(['./socket.js'], ['server']);
 })
 
-/**
- * $ gulp server
- * description: launch the node server.
- * If there's a server already running, kill it.
- */
-gulp.task('server', function() {
-    if (node) node.kill();
-    node = spawn('node', ['socket.js'], { stdio: 'inherit' });
-    node.on('close', function(code) {
-        if (code === 8) {
-            console.log('Error detected, waiting for changes...');
-        }
-    })
-})
+// /**
+//  * $ gulp server
+//  * description: launch the node server.
+//  * If there's a server already running, kill it.
+//  */
+// var node; // process
+// gulp.task('server', function() {
+//     if (node) {
+//         node.kill();
+//     }
+//     node = spawn('node', ['socket.js'], { stdio: 'inherit' });
+//     node.on('close', function(code) {
+//         if (code === 8) {
+//             console.log('Error detected, waiting for changes...');
+//         }
+//     });
+// })
 
-// clean up if an error goes unhandled.
-process.on('exit', function() {
-    if (node) node.kill();
-})
+// // clean up if an error goes unhandled.
+// process.on('exit', function() {
+//     if (node) {
+//         node.kill();
+//     }
+// })
